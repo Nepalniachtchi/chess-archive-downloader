9037f0ea6cea8452d62c97abcbc159cda1d16efe
Nepalniachtchi <nepal@inboxlite.com>
2021-09-10 20:18:04 +0100

    Clean up scripts.

--- scripts/add-tournament.py
+++ scripts/add-tournament.py
@@ -24,30 +24,11 @@ archive = Archive()
 # Titled Tuesday tournaments
 tournaments = [
     # August 2021
+    "titled-tuesday-blitz-august-31-2021-2546688",
     "titled-tuesday-blitz-august-24-2021-2533344",
     "titled-tuesday-blitz-august-17-2021-2528831",
     "titled-tuesday-blitz-august-10-2021-2514265",
     "titled-tuesday-blitz-august-03-2021-2500049",
-
-    # July 2021
-    "titled-tuesday-blitz-july-27-2021-2485661",
-    "titled-tuesday-blitz-july-20-2021-2471236",
-    "titled-tuesday-blitz-july-13-2021-2457070",
-    "titled-tuesday-blitz-july-06-2021-2443189",
-
-    # June 2021
-    "titled-tuesday-blitz-june-29-2021-2428655",
-    "titled-tuesday-blitz-june-22-2021-2413621",
-    "titled-tuesday-blitz-june-15-2021-2399019",
-    "titled-tuesday-blitz-june-08-2021-2384337",
-    "titled-tuesday-blitz-june-01-2021-2359358",
-
-    # May 2021
-    "titled-tuesday-blitz-may-25-2021-2333682",
-    "titled-tuesday-blitz-may-18-2021-2319376",
-    "titled-tuesday-blitz-may-11-2021-2314015",
-    "titled-tuesday-blitz-may-4-2021-2288851",
-
 ]
 for tournament in tournaments:
     archive.add_tournament(tournament)
--- scripts/get-monthly-games.py
+++ scripts/get-monthly-games.py
@@ -5,20 +5,10 @@ from chesscom.crawler import Crawler
 archive_date = sys.argv[1]
 
 crawler = Crawler()
-
 crawler \
     .set_date(archive_date) \
     .add_players(["hikaru"]) \
+    .add_tournaments([
+        "2021-chess-com-daily-chess-championship",
+    ]) \
     .crawl()
-
-# # Example of seeing players from tournamnents
-# crawler \
-#     .set_date(archive_date) \
-#     .add_players(["hikaru"]) \
-#     .add_tournaments([
-#         "titled-tuesday-blitz-july-27-2021-2485661",
-#         "titled-tuesday-blitz-july-20-2021-2471236",
-#         "titled-tuesday-blitz-july-13-2021-2457070",
-#         "titled-tuesday-blitz-july-06-2021-2443189",
-#     ]) \
-#     .crawl()
